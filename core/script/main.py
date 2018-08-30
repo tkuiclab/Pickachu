35d43e2c79e9e93ebfa8d4a2617e81a3d599dae9
NEET <zxc455233@gmail.com>
2018-08-30 16:13:15 +0800

    Create Empty Service in main.py

--- core/script/main.py
+++ core/script/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from accupick3d.msg import cmd
+from std_msgs.msg import Empty
+import std_srvs.srv
+#from accupick3d.msg import cmd
 
 def ScannerCmd(cmd_code):
   pub = rospy.Publisher('accupick3d/cmd', cmd, queue_size=1)
@@ -11,15 +13,22 @@ def ScannerCmd(cmd_code):
   print(msg)
   pub.publish(msg)
 
+def TakePicture(req):
+  print("Take a picture")
+  return std_srvs.srv.EmptyResponse()
+
 if __name__ == '__main__':
   try:
     rospy.init_node('main', anonymous=True)
     print("PickaChu~ Picka Picka~")
 
+    s = rospy.Service('take_picture', std_srvs.srv.Empty, TakePicture)
     ## 3D Scanner StartScan
-    ScannerCmd(254)
+    #ScannerCmd(254)
     ## Get Image and 3D Point Cloud from Scanner
     #ScannerCmd(255)
+
+    rospy.spin()
 
   except rospy.ROSInterruptException:
     pass
