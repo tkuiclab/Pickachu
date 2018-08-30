fd17ca93d95507f793c261d100ce8a9865c3fcaf
NEET <zxc455233@gmail.com>
2018-08-30 20:25:48 +0800

    Simple SOLOMON Control
    Service Server:/take_picture, /get_picture
    Srv: std_srvs/Empty
    
    Save image in <core>/images/image.jpg

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Pickachu
 2018 Taiwan Pickathon Challenge
+
+## Startup Process
+```bash
+$ source <WORKSPACE_OF_PICKACHU>/devel/setup.bash
+$ roslaunch accupick3d accupick3d.launch
+$ roslaunch core core.launch
+```
+
+## How to use
+```bash
+# Take a picture
+$ rosservice call /take_picture "{}"
+
+# Get & Save picture at <WORKSPACE_OF_PICKCHU>/src/core/images/
+$ rosservice call /get_picture "{}"
+```
--- core/script/main.py
+++ core/script/main.py
@@ -3,11 +3,11 @@ import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Empty
 import std_srvs.srv
-#from accupick3d.msg import cmd
+from accupick3d.msg import cmd
+
+pub = rospy.Publisher('accupick3d/cmd', cmd, queue_size=1)
 
 def ScannerCmd(cmd_code):
-  pub = rospy.Publisher('accupick3d/cmd', cmd, queue_size=1)
-
   msg = cmd()
   msg.cmd = cmd_code
   print(msg)
@@ -15,6 +15,12 @@ def ScannerCmd(cmd_code):
 
 def TakePicture(req):
   print("Take a picture")
+  ScannerCmd(254)
+  return std_srvs.srv.EmptyResponse()
+
+def GetPicture(req):
+  print("Get a picture")
+  ScannerCmd(255)
   return std_srvs.srv.EmptyResponse()
 
 if __name__ == '__main__':
@@ -22,7 +28,8 @@ if __name__ == '__main__':
     rospy.init_node('main', anonymous=True)
     print("PickaChu~ Picka Picka~")
 
-    s = rospy.Service('take_picture', std_srvs.srv.Empty, TakePicture)
+    s0 = rospy.Service('take_picture', std_srvs.srv.Empty, TakePicture)
+    s1 = rospy.Service('get_picture', std_srvs.srv.Empty, GetPicture)
     ## 3D Scanner StartScan
     #ScannerCmd(254)
     ## Get Image and 3D Point Cloud from Scanner
